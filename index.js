585c2795bba461f37ffb0a7f3ae3ec5db65f952c
RizzyFuzz <141845356+rizzlogy@users.noreply.github.com>
2023-10-27 12:48:43 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,19 @@
 const express = require('express');
 const app = express();
+const PORT = 8022 || 4500 || 3000;
 
 app.use(express.static('public'))
 app.use('/public', express.static('public'));
+app.set("json spaces", 2);
+app.set("trust proxy", true);
+app.use(express.urlencoded({ extended: true }));
+app.use(body.json({ type: "application/json" }));
+app.disable("x-powered-by");
+app.use((req, res, next) => {
+  res.setHeader("x-powered-by", "RizzyFuzz Backend");
+  next();
+});
+
 app.get('/eval', async (req, res) => {
   let code = req.query.query || req.query.q || req.query.code
 	let evaled
@@ -19,6 +30,8 @@ app.get('/', function(req, res) {
     res.send('Welcome to Node JS V1');
 });
 
-app.listen(process.env.PORT || 3000);
+app.listen(PORT, () => {
+  console.log("Server running on port " + PORT);
+});
 
 module.exports = app;
