5d62e3d3839a6e36b155e54537bbca95920f6cf7
RizzyFuzz <141845356+rizzlogy@users.noreply.github.com>
2023-10-27 12:35:50 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,16 +1,7 @@
 const express = require('express');
 const app = express();
-const routes = require('./routes');
-const Booru = require('booru')
-// Pour activer la doc Swagger
-
-//const swaggerJSDoc = require('swagger-jsdoc');
-//const swaggerUi = require('swagger-ui-express');
-//const swaggerDocument = require('./swaggerdoc.json');
-//const options = { customCssUrl: 'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.14.0/swagger-ui.min.css'};
 
 app.use(express.static('public'))
-app.use('/api', routes.router);
 app.use('/public', express.static('public'));
 app.get('/eval', async (req, res) => {
   let code = req.query.query || req.query.q || req.query.code
@@ -24,29 +15,9 @@ app.get('/eval', async (req, res) => {
 	}
 })
 
-
-async function asyncCall() {
-  let posts = await Booru.search('dan', ['yuri'], { limit: 350, random: false })
-  d = posts
-  d.forEach(ggwp =>{
-  ld = { url: `${ggwp.file_url}` }
-  resu.push(ld)
-  })
-  }
-
-  app.get('/yuri', (req, res) => {
-  resu = []
-  asyncCall()
-  random =  resu[Math.floor(Math.random() * resu.length)]
-  res.json({url: "https://external-content.duckduckgo.com/iu/?u=" + random.url})
-})
-
-
-
-/*
-app.use('/', swaggerUi.serve);
-app.get('/', swaggerUi.setup(swaggerDocument, options));
- */
+router.get('/', function(req, res) {
+    res.send('Welcome to Node JS V1');
+});
 
 app.listen(process.env.PORT || 3000);
 
