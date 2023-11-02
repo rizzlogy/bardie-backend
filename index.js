a6b239c45487303fa52e81095e66f37b215fb23b
RizzyFuzz <141845356+rizzlogy@users.noreply.github.com>
2023-11-02 23:02:51 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,9 @@
 const express = require('express');
 const body = require("body-parser");
-let { realistic } = require("./lib/stablediff");
 const Bard = require('./lib/bard');
 const app = express();
 const PORT = 8022 || 4500 || 3000;
 
-app.use(express.static('public'))
-app.use('/public', express.static('public'));
 app.set("json spaces", 2);
 app.set("trust proxy", true);
 app.use(express.urlencoded({ extended: true }));
@@ -16,18 +13,6 @@ app.use((req, res, next) => {
   res.setHeader("x-powered-by", "RizzyFuzz Backend");
   next();
 });
-
-app.get('/eval', async (req, res) => {
-  let code = req.query.query || req.query.q || req.query.code
-	let evaled
-	try {
-		evaled = await eval(`(async () => { ${code} })()`)
-	} catch (e) {
-		evaled = e
-	} finally {
-		res.send(require('util').format(evaled))
-	}
-})
 
 app.get("/bard", async (req, res) => {
   let prompts = req.query.prompt;
@@ -41,7 +26,7 @@ app.get("/bard", async (req, res) => {
   try {
     await bard.configure(1, "cgi0zjh5k1ckIk7VU6CZ9PaXwmZOXYz1mdI6Jg7zSuBk6QTCVHWEVsXbZGmowJHmQ4Epiw.");
 
-    const response = await bard.question(prompts);
+    const response = await bard.question(decodeURIComponent(prompts));
     res.json({ result: response, status: 200, creator: "RizzyFuzz" 
     });
   } catch (error) {
@@ -52,51 +37,10 @@ app.get("/bard", async (req, res) => {
       msg: "Internal Server Error!",
     });
   }
-})
-
-app.get("/stablediff", async (req, res) => {
-  let prompts = req.query.prompt;
-  if (!prompts)
-    return res.status(424).json({
-      status: 424,
-      creator: "RizzyFuzz",
-      msg: "No Prompt Provided",
-    });
-  try {
-    let { secondSpeed:second,
-	 model,
-	 prompt, 
-	 negative_prompt, 
-	 height,
-	 width, 
-	 resultImage 
-	} = await realistic(
-       "absolutereality_v181.safetensors [3d9d4d2b]",
-        prompts
-    );
-    res.json({ result: {
-	    speed,
-	    model,
-	    prompt,
-	    negative_prompt,
-	    height,
-	    width,
-	    resultImage	    
-    },
-	    status: 200, creator: "RizzyFuzz" 
-    });
-  } catch (e) {
-    console.log(e);
-    res.status(500).json({
-      status: 500,
-      creator: "RizzyFuzz",
-      msg: e.message,
-    });
-  }
 });
 
 app.get('/', function(req, res) {
-    res.send('Welcome to Node JS V1');
+    res.send('Welcome to Bard AI API!');
 });
 
 app.listen(PORT, () => {
