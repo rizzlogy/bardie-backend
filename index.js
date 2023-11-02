cfd4a6b2d22b7a328500cc3607f13f38470aa6a5
RizzyFuzz <141845356+rizzlogy@users.noreply.github.com>
2023-11-03 00:19:36 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,37 +1,36 @@
 const express = require('express');
-const body = require("body-parser");
 const Bard = require('./lib/bard');
 const app = express();
-const PORT = 8022 || 4500 || 3000;
+const PORT = process.env.PORT || 8022;
 
 app.set("json spaces", 2);
 app.set("trust proxy", true);
 app.use(express.urlencoded({ extended: true }));
-app.use(body.json({ type: "application/json" }));
+app.use(express.json());
 app.disable("x-powered-by");
 app.use((req, res, next) => {
   res.setHeader("x-powered-by", "RizzyFuzz Backend");
   next();
 });
 
-app.get("/api/onstage", async (req, res) => {
+app.post("/api/onstage", async (req, res) => {
   let ask = req.query.ask;
-  if (!ask)
+  if (!ask) {
     return res.status(400).json({
       status: 400,
       creator: "RizzyFuzz",
       msg: "Bad Request: No Query Ask Provided",
     });
+  }
+
   const bard = new Bard();
   try {
     await bard.configure(1, "cgi0zjh5k1ckIk7VU6CZ9PaXwmZOXYz1mdI6Jg7zSuBk6QTCVHWEVsXbZGmowJHmQ4Epiw.");
-
-    const response = await bard.question(ask);
-    res.json({ result: response, status: 200, creator: "RizzyFuzz" 
-    });
+    const response = await bard.question(decodeURIComponent(ask));
+    res.json({ result: response, status: 200, creator: "RizzyFuzz" });
   } catch (error) {
-    console.log(error);
-  res.status(500).json({
+    console.error(error);
+    res.status(500).json({
       status: 500,
       creator: "RizzyFuzz",
       msg: "Internal Server Error!",
@@ -40,15 +39,13 @@ app.get("/api/onstage", async (req, res) => {
 });
 
 app.get('/', function(req, res) {
-    res.status(200).json({
-      status: 200,
-      creator: "RizzyFuzz",
-      msg: "Server API ON!",
-    });
+  res.status(200).json({
+    status: 200,
+    creator: "RizzyFuzz",
+    msg: "Server API ON!",
+  });
 });
 
 app.listen(PORT, () => {
   console.log("Server running on port " + PORT);
 });
-
-module.exports = app;
