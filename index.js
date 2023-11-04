dc5b8ac13a07d61133995f01f818b4086305ea60
rizzlogy <rizzlogy@users.noreply.github.com>
2023-11-04 08:30:48 +0000

    automatic tidy up code

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const cors = require('cors')
+const cors = require("cors");
 const logger = require("morgan");
 const chalk = require("chalk");
 const Bard = require("./lib/bard");
