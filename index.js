98e3ac94c865e2fa661046bcc88a153b2dab5948
RizzyFuzz <141845356+rizzlogy@users.noreply.github.com>
2023-11-05 15:38:01 +0800

    Update index.js

--- index.js
+++ index.js
@@ -190,13 +190,13 @@ app.all("/status", async (req, res, next) => {
   });
 });
 
-app.use((req, res, next) =>
+app.use((req, res, next) => {
     res.status(404).json({
     content: "Not Found!",
     status: 500,
     creator: "RizzyFuzz",
   });
-);
+});
 
 app.listen(PORT, () => {
   console.log("Server running on port " + PORT);
