137b5aa0af676fc2e81e39c488eb25f474b20951
RizzyFuzz <141845356+rizzlogy@users.noreply.github.com>
2023-11-05 12:12:15 +0800

    Update index.js

--- index.js
+++ index.js
@@ -28,7 +28,20 @@ function formatBytes(bytes, decimals = 2) {
   const i = Math.floor(Math.log(bytes) / Math.log(k));
 
   return parseFloat((bytes / Math.pow(k, i)).toFixed(dm)) + " " + sizes[i];
-}
+};
+
+function runtime(seconds) {
+    seconds = Number(seconds);
+    var d = Math.floor(seconds / (3600 * 24));
+    var h = Math.floor((seconds % (3600 * 24)) / 3600);
+    var m = Math.floor((seconds % 3600) / 60);
+    var s = Math.floor(seconds % 60);
+    var dDisplay = d > 0 ? d + (d == 1 ? " day, " : " days, ") : "";
+    var hDisplay = h > 0 ? h + (h == 1 ? " hour, " : " hours, ") : "";
+    var mDisplay = m > 0 ? m + (m == 1 ? " minute, " : " minutes, ") : "";
+    var sDisplay = s > 0 ? s + (s == 1 ? " second" : " seconds") : "";
+    return dDisplay + hDisplay + mDisplay + sDisplay;
+};
 
 function status(code) {
   if (code > 400 && code < 499) return chalk.yellow(code);
@@ -36,7 +49,7 @@ function status(code) {
   if (code > 299 && code < 399) return chalk.cyan(code);
   if (code > 199) return chalk.green(code);
   return chalk.yellow(code);
-}
+};
 
 app.use(
   logger(function (tokens, req, res) {
@@ -165,6 +178,8 @@ Platform : ${os.platform()}
 Memory RAM :  ${formatBytes(os.totalmem() - os.freemem())} / ${formatBytes(
     os.totalmem(),
   )}
+<br>
+Runtime Server : ${runtime(os.uptime())}
 </hr>
 </center>
 </body>
