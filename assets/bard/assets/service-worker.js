4d775612234aa2a6422b16320f142938f8153ce5
rizzlogy <rizzlogy@users.noreply.github.com>
2023-11-11 18:32:03 +0000

    automatic tidy up code

--- assets/bard/assets/service-worker.js
+++ assets/bard/assets/service-worker.js
@@ -1,33 +1,31 @@
 // service-worker.js
 
-const CACHE_NAME = 'bardie-cache-v1';
+const CACHE_NAME = "bardie-cache-v1";
 const urlsToCache = [
-  '/',
-  '/manifest.json',
-  '/assets/img/icon.png',
-  '/assets/css/bard.css',
-  '/assets/js/bard.js',
-  'https://fonts.googleapis.com/css2?family=Material+Symbols+Rounded:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200',
-  'https://cdn.jsdelivr.net/npm/sweetalert2@10',
+  "/",
+  "/manifest.json",
+  "/assets/img/icon.png",
+  "/assets/css/bard.css",
+  "/assets/js/bard.js",
+  "https://fonts.googleapis.com/css2?family=Material+Symbols+Rounded:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200",
+  "https://cdn.jsdelivr.net/npm/sweetalert2@10",
 ];
 
-self.addEventListener('install', (event) => {
+self.addEventListener("install", (event) => {
   event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then((cache) => cache.addAll(urlsToCache))
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(urlsToCache)),
   );
 });
 
-self.addEventListener('fetch', (event) => {
+self.addEventListener("fetch", (event) => {
   event.respondWith(
-    caches.match(event.request)
-      .then((response) => {
-        return response || fetch(event.request);
-      })
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    }),
   );
 });
 
-self.addEventListener('activate', (event) => {
+self.addEventListener("activate", (event) => {
   event.waitUntil(
     caches.keys().then((cacheNames) => {
       return Promise.all(
@@ -35,8 +33,8 @@ self.addEventListener('activate', (event) => {
           if (cacheName !== CACHE_NAME) {
             return caches.delete(cacheName);
           }
-        })
+        }),
       );
-    })
+    }),
   );
 });
