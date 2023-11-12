65ce608c3fd71ae6edb793117db5f9324edca90e
RizzyFuzz <141845356+rizzlogy@users.noreply.github.com>
2023-11-12 13:04:42 +0800

    Update service-worker.js

--- assets/bard/assets/js/service-worker.js
+++ assets/bard/assets/js/service-worker.js
@@ -1,40 +1,41 @@
-const CACHE_NAME = "bardie-v1";
+const CACHE_NAME = 'bardie-cache-v1';
 const urlsToCache = [
-  "/",
-  "/chat",
-  "/assets/img/icon.png",
-  "/assets/js/manifest.json",
-  "/assets/css/bard.css",
-  "/assets/js/bard.js",
-  "https://fonts.googleapis.com/css2?family=Material+Symbols+Rounded:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200",
-  "https://cdn.jsdelivr.net/npm/sweetalert2@10",
+  '/',
+  '/chat',
+  '/assets/js/manifest.json',
+  '/assets/img/icon.png',
+  '/assets/css/bard.css',
+  '/assets/js/bard.js',
+  'https://fonts.googleapis.com/css2?family=Material+Symbols+Rounded:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200',
+  'https://cdn.jsdelivr.net/npm/sweetalert2@10',
 ];
 
-self.addEventListener("install", (event) => {
+self.addEventListener('install', (event) => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => cache.addAll(urlsToCache)),
+    caches.open(CACHE_NAME)
+      .then((cache) => cache.addAll(urlsToCache))
   );
 });
 
-self.addEventListener("fetch", (event) => {
+self.addEventListener('fetch', (event) => {
   event.respondWith(
-    caches.match(event.request).then((response) => {
-      return response || fetch(event.request);
-    }),
+    caches.match(event.request)
+      .then((response) => {
+        return response || fetch(event.request);
+      })
   );
 });
 
-self.addEventListener("activate", (event) => {
-  // Remove old caches
+self.addEventListener('activate', (event) => {
   event.waitUntil(
-    (async () => {
-      const keys = await caches.keys();
-      return keys.map(async (cache) => {
-        if (cache !== cacheName) {
-          console.log("Service Worker: Removing old cache: " + cache);
-          return await caches.delete(cache);
-        }
-      });
-    })(),
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          if (cacheName !== CACHE_NAME) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
   );
 });
