b2a8ea33939aa8a8bff778fea4a9816b7c9f5df0
RizzyFuzz <141845356+rizzlogy@users.noreply.github.com>
2023-11-21 13:42:55 +0700

    Update service-worker.js

--- assets/bard/assets/js/service-worker.js
+++ assets/bard/assets/js/service-worker.js
@@ -3,10 +3,6 @@ const CACHE_NAME = "bardie-cache-v1";
 const urlsToCache = [
   "/",
   "/chat",
-  "/assets/js/manifest.json",
-  "/assets/img/icon.png",
-  "/assets/css/bard.css",
-  "/assets/js/bard.js",
   "https://fonts.googleapis.com/css2?family=Material+Symbols+Rounded:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200",
   "https://cdn.jsdelivr.net/npm/sweetalert2@10",
 ];
