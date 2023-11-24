1352be5edfd1b5221e21cb77cd2e031726222486
rizzlogy <rizzlogy@users.noreply.github.com>
2023-11-24 17:02:54 +0000

    automatic tidy up code

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-process.env['NODE_TLS_REJECT_UNAUTHORIZED'] = '0'
+process.env["NODE_TLS_REJECT_UNAUTHORIZED"] = "0";
 const bodyParser = require("body-parser");
 const express = require("express");
 const cors = require("cors");
