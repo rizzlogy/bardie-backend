9e4a67a6bfd9df73c95a0c1567ba0c8a51b44f7b
RizzyFuzz <141845356+rizzlogy@users.noreply.github.com>
2023-12-14 23:14:45 +0800

    Update index.js

--- index.js
+++ index.js
@@ -29,7 +29,7 @@ function formatBytes(bytes, decimals = 2) {
   const i = Math.floor(Math.log(bytes) / Math.log(k));
 
   return parseFloat((bytes / Math.pow(k, i)).toFixed(dm)) + " " + sizes[i];
-},
+}
 
 function runtime(seconds) {
   seconds = Number(seconds);
